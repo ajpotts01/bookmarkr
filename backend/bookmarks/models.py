754b3d925025d93d61d89447a7cfbd07dc6288a7
Alexander Potts <ajpotts01@gmail.com>
2025-05-10 23:29:34 +1000

    feat(backend): setup

---
+++ backend/bookmarks/models.py
@@ -0,0 +1,19 @@
+# Standard lib imports
+import uuid
+
+# Third-party imports
+from django.db import models
+
+
+class Bookmark(models.Model):
+    uuid: models.UUIDField = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    title: models.CharField = models.CharField(max_length=150)
+    url: models.URLField = models.URLField()
+    created_at: models.DateTimeField = models.DateTimeField(auto_now_add=True)
+    updated_at: models.DateTimeField = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural: str = "bookmarks"
+
+    def __str__(self) -> str:
+        return f"{self.title} ({self.url})"
